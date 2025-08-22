acda6453f99a17e19dcaaf174df1a304341a3773
56879-e <ebrahimredaelshahed@gmail.com>
2025-08-22 11:41:14 -0700

    Add files via upload

---
+++ classPage.js
@@ -0,0 +1,7 @@
+document.getElementById('videos-button').addEventListener('click', () => {
+    window.location.href = '/videosPage.html'; // Replace with the actual path to the videos page
+});
+
+document.getElementById('files-button').addEventListener('click', () => {
+    window.location.href = '/filesPage.html'; // Replace with the actual path to the files page
+});
