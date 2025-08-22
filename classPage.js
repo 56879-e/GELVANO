c43a96a531c3645cbb7d95637c26162b6eb95599
56879-e <ebrahimredaelshahed@gmail.com>
2025-08-22 16:20:34 -0700

    Delete classPage.js

--- classPage.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('videos-button').addEventListener('click', () => {
-    window.location.href = '/videosPage.html'; // Replace with the actual path to the videos page
-});
-
-document.getElementById('files-button').addEventListener('click', () => {
-    window.location.href = '/filesPage.html'; // Replace with the actual path to the files page
-});
